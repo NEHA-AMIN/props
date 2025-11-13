5f9248c74d6d2b9540bba278fe46018c0d56d31f
kroolo-anuj <145868361+NEHA-AMIN@users.noreply.github.com>
2025-11-13 16:10:13 +0530

    changes-v3

--- revamp_test/app/industries/page.tsx
+++ revamp_test/app/industries/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import NewIndustriesPage from '../components/NewIndustriesPage';
+import SignalSection from '../components/SignalSection';
 
 export default function Industries() {
-  return <NewIndustriesPage />;
+  return (
+    <>
+      <NewIndustriesPage />
+      <SignalSection />
+    </>
+  );
 }
--- revamp_test/app/resources/page.tsx
+++ revamp_test/app/resources/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { ResourcesPage } from '../components/ResourcesPage';
+import SignalSection from '../components/SignalSection';
 
 export default function Page() {
-  return <ResourcesPage />;
+  return (
+    <>
+      <ResourcesPage />
+      <SignalSection />
+    </>
+  );
 }
